24ba1c8b1c096812f7b6f99de8bb00f170b96040
Dheeraj Bhatia <dheerajb@buffalo.edu>
2021-03-19 22:56:33 +0000

    removed console.log and any unuseful code

--- routes.js
+++ routes.js
@@ -30,8 +30,9 @@ router.get('/logout', function (req, res) {
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated())
-    return next();
-  res.redirect('/');
+      return next()
+    
+  res.redirect('./');
 }
 
 module.exports = router;
